690e2ca715959e1d133eefde0cb9e55a64edbb9b
whiteboardenterprise <github@susantswain.com>
2025-06-19 18:54:20 +0530

    List filtration added

---
+++ listFilteration.py
@@ -0,0 +1,3 @@
+vehicles = ["Cycle", "Motorcycle", "Tacsy", "Autorikshaw", "Bus", "Train", "Airplain"]
+filteredVehicles = [vehicle for vehicle in vehicles if vehicle != "Airplain"]
+print(filteredVehicles)
