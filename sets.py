8c5416d946c3037ef64f1b811d8d67565d93a82e
whiteboardenterprise <github@susantswain.com>
2025-06-25 18:47:16 +0530

    Sets added

---
+++ sets.py
@@ -0,0 +1,12 @@
+nums = {1,2,3,4,5,6,7,8,9}
+print(nums)
+print("Adding 10 to the set")
+nums.add(10)
+print(nums)
+print("Removing 10 from the set")
+nums.remove(10)
+print(nums)
+print("Looping through the set...")
+for x in nums:
+ print(f"The item is {x}")
+
