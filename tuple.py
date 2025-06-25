0dec02cdb0c1ca43175e3d8973d6b60059790a6c
whiteboardenterprise <github@susantswain.com>
2025-06-25 18:29:33 +0530

    Tuple added

---
+++ tuple.py
@@ -0,0 +1,6 @@
+location = ("Delhi", "Mumbai", "Kolkata", "Chenai")
+print("Accessing Mumbai from the tuple")
+print(location[1])
+print("Looping through the tuple...")
+for x in location:
+ print(f"the item is {x}")
